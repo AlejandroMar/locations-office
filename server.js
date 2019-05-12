140f7cdeace85875ae7de801f7930e7f4a83fe31
Alejo <motungo@hotmail.com>
2019-05-12 16:16:01 +0200

    clean server

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ mongoose.connect(
     process.env.MONGO_DB_URL,
     { useNewUrlParser: true }
 )
-    .then(() => console.log('MongoDB Connected'))
+    .then(() => debug(chalk.magenta('MongoDB Connected')))
     .catch(err => debug(err));
 
 
