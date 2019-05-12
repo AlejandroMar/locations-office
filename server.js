1934ccf5fa0b32962cb4b56037c9ef68e5a39d07
Alejo <motungo@hotmail.com>
2019-05-12 14:08:29 +0200

    organize server.js code

--- server.js
+++ server.js
@@ -6,29 +6,33 @@ const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
+// Mount routes
+const locationsRoute = require('./routes/locationsRoute');
+
+// APP
+const app = express();
+
+// middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(morgan('dev'));
+
 mongoose.set('debug', true);
 mongoose.Promise = Promise;
 
-mongoose.connect('mongodb://alejo:alejo123@ds155606.mlab.com:55606/locations-api', { useNewUrlParser: true })
+mongoose.connect(
+    'mongodb://alejo:alejo123@ds155606.mlab.com:55606/locations-api',
+    { useNewUrlParser: true }
+)
     .then(() => console.log('MongoDB Connected'))
     .catch(err => debug(err));
 
 
-const locationsRoute = require('./routes/locationsRoute');
-
-
-const app = express();
-
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-
-
-app.use(morgan('dev'));
+// Use routes
+app.use('/locations', locationsRoute);
 
 
 const PORT = process.env.PORT || 4000;
-app.use('/locations', locationsRoute);
-
 app.listen(PORT, () => {
     debug(`listening on port ${chalk.yellow(PORT)}`);
 });
