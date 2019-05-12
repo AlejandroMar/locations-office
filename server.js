ec20026d8be0611f25fa6637e6cf5e3a241046b9
Alejo <motungo@hotmail.com>
2019-05-12 16:22:28 +0200

    mongoDB Connected chalk color blue

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ mongoose.connect(
     process.env.MONGO_DB_URL,
     { useNewUrlParser: true }
 )
-    .then(() => debug(chalk.magenta('MongoDB Connected')))
+    .then(() => debug(chalk.blue('MongoDB Connected')))
     .catch(err => debug(err));
 
 
