3dd2350eb20b67e2592656f03fb22450140561ef
Alejo <motungo@hotmail.com>
2019-05-12 16:10:57 +0200

    locationsRoutes

--- routes/locationsRoute.js
+++ routes/locationsRoute.js
@@ -1,19 +1,14 @@
 const express = require('express');
-const multer = require('multer');
 
 const router = express.Router();
 
-const storage = multer.memoryStorage();
-const upload = multer({ storage });
-
-
 const locationsController = require('../controllers/locationsController');
 
-router.get('/', async (req, res) => {
-    res.send('hello');
-});
 
-router.post('/', upload.single('avatar'), locationsController.postLocation);
+router.get('/all', locationsController.getLocations);
+
+router.get('/:id?', locationsController.getLocationById);
+router.get('/:name?', locationsController.getLocationByName);
 
 
 module.exports = router;
